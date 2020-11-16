47e5ab0916eb41860dead3d235c74b0ff37561ce
Gamb <d.namur.n@gmail.com>
2020-11-15 21:42:46 -0300

    Creando clase de modelo pelicula con sobreescritura de toString

---
+++ cineapp/src/main/java/cl/danielnamur/app/controller/HomeController.java
@@ -0,0 +1,44 @@
+package cl.danielnamur.app.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value="/home", method=RequestMethod.GET)
+	public String goHome(){
+		return "home";
+	}
+	
+	@RequestMapping(value = "/", method=RequestMethod.GET)
+	public String mostrarPrincipal(Model model) {
+		
+		List<String> peliculas = new LinkedList<>();
+		peliculas.add("rapido y furioso");
+		peliculas.add("el aro");
+		peliculas.add("aliens");
+		model.addAttribute("peliculas", peliculas);
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/detail")
+	public String mostrarDetalle(Model model) {
+		String tituloPelicula = "Rapido y furiosos";
+		int duracion = 136;
+		double precioEntrada = 2000;
+		
+		model.addAttribute("titulo", tituloPelicula);
+		model.addAttribute("duracion", duracion);
+		model.addAttribute("precio", precioEntrada);
+		
+		return "detalle";
+	}
+	
+}
